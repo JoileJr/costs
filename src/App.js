e2f20422971d966864f6f6ed8b5356989f8594df
JoileJr <joilejunior2@gmail.com>
2023-03-30 14:20:12 -0300

    services e edição de projetos

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import NewProject from "./components/pages/NewProject";
 import Projects from "./components/pages/Projects";
+import Project from "./components/pages/Project";
 
 
 import Container from "./components/layout/Container";
@@ -22,6 +23,7 @@ function App() {
           <Route exact path="/projects" element={ <Projects/> }/> 
           <Route exact path="/contact" element={ <Contact/> }/> 
           <Route exact path="/newproject" element={ <NewProject/> }/>
+          <Route exact path="/projects/:id" element={ <Project/> }/>
         </Routes>
       </Container> 
       <Footer/>
--- src/components/project/ProjectsCard.js
+++ src/components/project/ProjectsCard.js
@@ -20,7 +20,7 @@ function ProjectsCard({id, name, budget, category, handleRemove}){
         <span className={`${styles[category.toLowerCase()]}`}></span> {category}
       </p>
       <div className={styles.project_card_actions}>
-        <Link to={'/project/' + id}>
+        <Link to={'/projects/' + id}>
           <BsPencil /> Editar
         </Link>
         <button onClick={remove}>
